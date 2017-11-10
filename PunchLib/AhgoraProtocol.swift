d8d5eae916a1d814e751420191445bf102308553
Rafael Leandro <rafaelleandro@me.com>
2017-11-10 17:58:45 -0200

    New code struct

---
+++ PunchLib/AhgoraProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  AhgoraProtocol.swift
+//  PunchLib
+//
+//  Created by Rafael Leandro on 10/11/17.
+//  Copyright © 2017 Involves. All rights reserved.
+//
+
+import UIKit
+
+class AhgoraProtocol: NSObject {
+
+}
---
+++ PunchLib/HTTPRequest.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPRequest.swift
+//  PunchLib
+//
+//  Created by Rafael Leandro on 10/11/17.
+//  Copyright © 2017 Involves. All rights reserved.
+//
+
+import UIKit
+
+class HTTPRequest: NSObject {
+
+}
